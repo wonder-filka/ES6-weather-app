cc3f070a0b4fbf37ea90244883d38ce96bee20a9
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-08-18 18:18:39 +0300

    design + unit conversion + describes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 let h1 = document.querySelector(".currentInfo h1");
-let h6 = document.querySelector(".currentInfo h6");
-let h2Temp = document.querySelector(".currentInfo h2 #temp");
+let timeNow = document.querySelector(".currentInfo h5");
+let Temp = document.querySelector("span#temp");
 let inputSearch = document.querySelector(".input-group #search");
 let inputCurrent = document.querySelector(".input-group #current");
 let inputInfo = document.querySelector(".input-group #formControl");
+let cuttentIcon = document.querySelector("#icon");
+let currentDescription = document.querySelector(".currentInfo h4");
+let currentHumidity = document.querySelector("#humidity");
+let currentWind = document.querySelector("#wind");
+let currentFeelslike = document.querySelector("#feelslike");
+let celsTemp = document.querySelector("#cels");
+let farTemp = document.querySelector("#far");
+let celsiusTemp = null;
 let now = new Date();
 let apiKey = "ba322d86c6e375290a924f7f5aba942e";
+let newApi = "DPDT2QJYWGH8EYB67YK2R8G99";
 inputSearch.addEventListener("click", getSearchInfo);
 inputCurrent.addEventListener("click", getCurrentInfo);
+celsTemp.addEventListener("click", formatToCell);
+farTemp.addEventListener("click", formattoFar);
 
-h6.innerHTML = dayTime();
+timeNow.innerHTML = dayTime();
 
 function dayTime() {
   let days = [
@@ -29,14 +40,29 @@ function dayTime() {
 
 function getSearchInfo(event) {
   event.preventDefault();
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputInfo.value}&units=metric&appid=${apiKey}`;
+  let apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputInfo.value}?iconSet=icons2&unitGroup=metric&key=${newApi}&contentType=json`;
   axios.get(apiUrl).then(ShowTheTemperature);
 }
 
 function ShowTheTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  h2Temp.innerHTML = temperature;
-  h1.innerHTML = `${response.data.name}`;
+  let temperature = Math.round(response.data.currentConditions.temp);
+  celsiusTemp = temperature;
+  let cityName = response.data.timezone.split("/")[1];
+  let pics = response.data.currentConditions.icon;
+  let description = response.data.description;
+  let humidity = response.data.currentConditions.humidity;
+  let wind = response.data.currentConditions.windspeed;
+  let feelslike = response.data.currentConditions.feelslike;
+  Temp.innerHTML = temperature;
+  currentDescription.innerHTML = description;
+  h1.innerHTML = `${cityName}`;
+  currentHumidity.innerHTML = humidity;
+  currentWind.innerHTML = wind;
+  currentFeelslike.innerHTML = feelslike;
+  cuttentIcon.setAttribute(
+    "src",
+    `img/${response.data.currentConditions.icon}.svg`
+  );
 }
 
 function getCurrentInfo(event) {
@@ -47,6 +73,21 @@ function getCurrentInfo(event) {
 function getCurrentPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  let apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}?iconSet=icons2&unitGroup=metric&key=${newApi}`;
   axios.get(apiUrl).then(ShowTheTemperature);
+}
+
+function formatToCell(event) {
+  event.preventDefault();
+}
+
+function formattoFar(event) {
+  event.preventDefault();
+  let fahrenhei = Math.round((celsiusTemp * 9) / 5 + 32);
+  Temp.innerHTML = fahrenhei;
+}
+
+function formatToCell(event) {
+  event.preventDefault();
+  Temp.innerHTML = celsiusTemp;
 }
